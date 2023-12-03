54c29e6fdd0542370890a7550a50178524608874
tomaszmamala <119527491+tomaszmamala@users.noreply.github.com>
2023-12-03 21:25:21 +0100

    Update delete_hotel.php

--- projekt/delete_hotel.php
+++ projekt/delete_hotel.php
@@ -7,14 +7,14 @@ use Entity\Hotel;
 <?php
 require_once 'Hotel.php';
 
-if ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['id'])) {
-    $hotelId = $_GET['id'];
+if ($_SERVER["REQUEST_METHOD"] == 'POST') {
+    $hotelId = $_POST['hotelId'] ?? null;
 
     $hotel = new Hotel();
 
     $hotel->deleteHotel($hotelId);
 
-    $newUrl = 'all_hotels_admin.php?' . http_build_query(['message' => 'deleted', 'id' => $hotelId]);
+    $newUrl = 'all_hotels_admin.php?' . http_build_query(['message' => 'deleted']);
     header("Location: $newUrl");
     echo "Hotel został usunięty.";
 } else {
